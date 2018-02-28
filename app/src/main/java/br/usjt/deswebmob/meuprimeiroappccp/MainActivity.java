fc41f18c0f5bba2aa62d78cf7607a4b8d485519b
Victor <vf.201516754@gmail.com>
2018-02-28 20:53:45 -0300

    update app

--- app/src/main/java/br/usjt/deswebmob/meuprimeiroappccp/MainActivity.java
+++ app/src/main/java/br/usjt/deswebmob/meuprimeiroappccp/MainActivity.java
@@ -1,13 +1,17 @@
 package br.usjt.deswebmob.meuprimeiroappccp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.accessibility.AccessibilityManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    public static final String MENSAGEM = "br.usjt.deswebmob.meuprimeiroappccp.mensagem";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +20,13 @@ public class MainActivity extends Activity {
 
     }
 
-
     public void sendMessage(View view) {
+        EditText editText;
+        editText = (EditText) findViewById(R.id.edit_text);
+        String mensagem = editText.getText().toString();
 
+        Intent intent = new Intent(MainActivity.this, DisplayMessageActivity.class);
+        intent.putExtra(MENSAGEM, mensagem);
+        startActivity(intent);
     }
 }
