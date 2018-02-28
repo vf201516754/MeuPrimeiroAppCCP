f3b87bb0d3d9fe324f2e2d7e43d14f9474c81392
arqdsis <arqdsis@mocli10220.animaedu.intranet>
2018-02-28 20:17:33 -0300

    Primeiro Commit

---
+++ app/src/main/java/br/usjt/deswebmob/meuprimeiroappccp/MainActivity.java
@@ -0,0 +1,23 @@
+package br.usjt.deswebmob.meuprimeiroappccp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.accessibility.AccessibilityManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+    public void sendMessage(View view) {
+
+    }
+}
